277de87b95a1f9e85f43570b5c5edc7ed61e3388
noieeen <tharnnn1401@gmail.com>
2022-10-02 22:59:20 +0700

    LoginForm

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,57 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <h1>Hello</h1>
+    <h2>count1: {{ count1 }}</h2>
+    <hr />
+    <h2>count2: {{ count2 }}</h2>
+    <button @click="onClickAdd1">Add 1</button>
+    <button @click="onClickAdd2">Add 2</button>
+    <hr />
+    <h4>account: {{ states.account }}</h4>
+    <button @click="onClickClear">Clear Account</button>
+
+    <LoginForm/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent, ref, reactive, onMounted } from "vue";
+import LoginForm from '@/components/LoginForm.vue'
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+const defaultAcc = { username: "", password: "" };
+export default defineComponent({
+    setup() {
+        let count1 = 10;
+        const count2 = ref<number>(32);
+        const states = reactive({
+            account: { username: "admin", password: "1234" },
+        });
+        onMounted(() => {
+            console.log("Ready");
+        });
+        const onClickAdd1 = () => {
+            count1++;
+            console.log(count1);
+        };
+        const onClickAdd2 = () => {
+            count2.value++;
+            console.log(count2.value);
+        };
+        const onClickClear = () => {
+            // account.username=''
+            // account.password=''
+            states.account = defaultAcc;
+            console.log(states.account);
+        };
+        return {
+            count1,
+            count2,
+            onClickAdd1,
+            onClickAdd2,
+            states,
+            onClickClear,
+        };
+    },
+    components: { LoginForm }
+});
+</script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+            <h1>Login</h1>
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        return{}
+    },
+})
+</script>
